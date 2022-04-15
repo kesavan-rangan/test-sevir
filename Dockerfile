c791c0425378d50d8e15f6bdedffd0f3b8b453cb
Kesavan Rangan <68580746+kesavan-rangan@users.noreply.github.com>
2022-04-15 11:33:06 +0530

    Adding helper file for S3 integration

--- Dockerfile
+++ Dockerfile
@@ -3,20 +3,23 @@ ARG MODEL_DIR="t5-small"
 
 # Creating a base image to install all the App dependencies
 FROM python:slim-buster as build-image
+#FROM huggingface/transformers-pytorch-cpu as build-image
 RUN apt-get update && apt-get install -y g++ make cmake unzip libcurl4-openssl-dev
 ARG FUNCTION_DIR
 RUN mkdir -p ${FUNCTION_DIR}
 COPY app/* ${FUNCTION_DIR}
 COPY requirements.txt ${FUNCTION_DIR}
+RUN pip uninstall --yes jupyter
 RUN pip install --target ${FUNCTION_DIR} -r ${FUNCTION_DIR}/requirements.txt
 RUN pip install --target ${FUNCTION_DIR} awslambdaric
 
 # Using the above image that has the installed libraries to be used in AWS Lambda's container image 
 FROM python:slim-buster
+#FROM huggingface/transformers-pytorch-cpu
 ARG FUNCTION_DIR
 ARG MODEL_DIR
 WORKDIR ${FUNCTION_DIR}
 COPY --from=build-image ${FUNCTION_DIR} ${FUNCTION_DIR}
-RUN mkdir -p ${MODEL_DIR} && mv config.json ${MODEL_DIR}
+#RUN mkdir -p ${MODEL_DIR} && mv config.json ${MODEL_DIR}
 ENTRYPOINT [ "/usr/local/bin/python", "-m", "awslambdaric" ]
 CMD [ "main.handler" ]
--- app/main.py
+++ app/main.py
@@ -1,126 +1,61 @@
+from helper import download_from_s3, extract_zip_file
 from re import template
 import pandas as pd
 import json
 import time
 import uvicorn
-import os
-import shutil
 from fastapi import FastAPI, Request
+#from spacy import displacy
 from random import sample
 from transformers import pipeline
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from mangum import Mangum
+from transformers import AutoConfig, AutoModelForSeq2SeqLM
 import os
-from transformers import AutoConfig
 
 
-# def parse_dataframe(df):
-#     result = df.to_json(orient="records")
-#     parsed=json.loads(result)
-#     return parsed
+S3_BUCKET = "sevir-models"
+S3_PATH = "models/t5-small.zip"
 
+MODEL_NAME = "t5-small"
+
+MODEL_FILE_NAME = f"{MODEL_NAME}.zip"
+MODEL_PATH = "/tmp/models"
+file_name = os.path.join(MODEL_PATH, MODEL_FILE_NAME)
+if not os.path.exists(MODEL_PATH):
+    os.makedirs(MODEL_PATH)
+
+# Downloads file from S3
+download_from_s3(S3_BUCKET, S3_PATH, file_name)
+
+# Unzips the downloaded Zip file
+extract_zip_file(file_name)
+
+MODEL_LOCATION = os.path.join(MODEL_PATH, MODEL_NAME)
 
 app = FastAPI(
     title = "NEW API",
     description = "API helps analyze news articles"
 )
 
-#app.mount("/static", StaticFiles(directory="static"), name="static")
-print("DEBUGG")
-print(os.getcwd())
+print("DEBUGGG -- 1")
 print(os.listdir())
+print("DEBUGGG -- 2")
+print(os.listdir(MODEL_LOCATION))
 
-model = "t5-small"
-model_path = os.path.join(os.getcwd(), model) 
-if not os.path.exists(model_path):
-    os.makedirs(model_path)
-    if os.path.exists("config.json"):
-            shutil.move("config.json", model_path)
-    print("DEBUGG -- 2")
-    print(os.listdir(model_path))
-
-# summarizer = pipeline("summarization", model=model, tokenizer=model)
-config = AutoConfig.from_pretrained('t5-small')
-
-summarizer = pipeline("summarization")
-
-
-#nlp = spacy.load("en_core_web_sm")
+# model = AutoModelForSeq2SeqLM.from_pretrained("MODEL_LOCATION")
+summarizer = pipeline("summarization", model=MODEL_LOCATION)
 
 file = pd.read_csv("./eventnarratives.csv")
-#print(file.head())
-#file.head()
 
-# API
-@app.get("/")
+
+@app.get("/welcome")
 def read_root():     
     return {"message": "Welcome from the Weather Application"}
 
 
-#NER
-# row = file.sample()
-# episode_narrative = row['EPISODE_NARRATIVE'].values
-# event_narrative = str(row['EVENT_NARRATIVE'])
-
-# def ner_spacy(text):
-#     doc1 = nlp(text)
-#     for ent in doc1.ents:
-#         yield ent.text, ent.label_
-
-
-# @app.post("/ner")
-# def ner():
-#     row = file.sample()
-#     episode_narrative = row['EPISODE_NARRATIVE'].values
-#     #event_narrative = str(row['EVENT_NARRATIVE'])
-#     episode_str = str(episode_narrative)
-
-#     doc = nlp(episode_str)
-#     ents=[]
-#     #ner1 = displacy.render(doc, style='ent', jupyter=True)
-#     for ent in doc.ents:
-#             ents.append({"text": ent.text, "label": ent.label_})
-
-#     return {"ents": ents}
-
-# templates = Jinja2Templates(directory='app')
-
-# @app.post("/display_ner", response_class=HTMLResponse)
-# def display_ner():
-#     row = file.sample()
-#     episode_narrative = row['EPISODE_NARRATIVE'].values
-#     episode_str = str(episode_narrative)
-
-#     doc1 = nlp(episode_str)
-#     return templates.TemplateResponse("item.html")
-
-
-# episode_str = str(episode_narrative)
-
-# print(event_narrative)
-# A weak low pressure area brought a period of sleet and freezing rain to southern WI. Up to one half inch of sleet accumulation and a tenth of an inch of ice accumulation occurred. Some vehicle slide-offs and accidents occurred.  There were a small number of power outages mostly in far southeast WI.
-# txt = "An area of low pressure over southern Manitoba the morning of the 15th moved to near southern Hudson Bay by the morning of the 16th. A cold front associated with this low moved across the Northland during the afternoon and evening hours of the 15th. A warm front had moved through on the previous day brought a warm and unstable airmass in its wake. The cold front then worked with this airmass to create severe storms from northeast Minnesota into northwest Wisconsin and western Lake Superior. Damaging winds accounted for most of the reports with these storms, although there was some marginally severe hail as well. Most of the damage was to trees, but some property damage to pontoon boats and a home also occurred."
-
-# nlp = spacy.load("en_core_web_sm")
-
-# doc = nlp(episode_str)
-
-# displacy.render(doc, style='ent', jupyter=True)
-
-#SUMMARIZE
-# from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-#summarizer = pipeline(task="summarization", model="sshleifer/distilbart-cnn-6-6", tokenizer="sshleifer/distilbart-cnn-6-6",framework="pt")
-
-# summarizer2 = pipeline("summarization",model="t5-small", tokenizer="t5-small")
-
-# def summarize(text):
-#     summary = summarizer2(text, max_length=40, min_length=20, do_sample=False)
-#     return summary
-
-# summarize(episode_str)
-
-@app.post("/{row_id}/summarizer")
+@app.get("/summarizer/{row_id}")
 def summarizers(row_id: int):
     #start_time = time.time()
 
